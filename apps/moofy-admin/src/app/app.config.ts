0dab4da80a6c46a677415279d753cd5a8a027c6c
claudio <54946311+cvandradg@users.noreply.github.com>
2024-11-28 11:17:32 -0600

    fix: attempting to fix hosting

--- apps/moofy-admin/src/app/app.config.ts
+++ apps/moofy-admin/src/app/app.config.ts
@@ -1,13 +1,24 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
 import { appRoutes } from './app.routes';
+import { provideRouter } from '@angular/router';
+import { environment } from '@moofy-admin/shared';
+import { FIREBASE_OPTIONS } from '@angular/fire/compat';
 import { provideClientHydration } from '@angular/platform-browser';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { provideAnimations } from '@angular/platform-browser/animations';
+import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 export const appConfig: ApplicationConfig = {
   providers: [
+    provideAnimations(),
     provideClientHydration(),
+    provideRouter(appRoutes),
+    provideAnimationsAsync(),
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(appRoutes), provideAnimationsAsync(), provideAnimationsAsync(),
+    provideFirebaseApp(() => initializeApp(environment.firebaseConfig)),
+    {
+      provide: FIREBASE_OPTIONS,
+      useValue: environment.firebaseConfig,
+    },
   ],
 };
