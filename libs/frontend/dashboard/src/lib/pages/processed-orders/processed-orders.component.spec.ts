f2678eb0b17277e9b7860e2832256d9f5aa58fdf
claudio <54946311+cvandradg@users.noreply.github.com>
2024-12-10 03:28:19 -0600

    fix: test

--- libs/frontend/dashboard/src/lib/pages/processed-orders/processed-orders.component.spec.ts
+++ libs/frontend/dashboard/src/lib/pages/processed-orders/processed-orders.component.spec.ts
@@ -1,5 +1,8 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { ProcessedOrdersComponent } from './processed-orders.component';
+import { MODULES } from '@moofy-admin/shared';
+import { CommonModule, CurrencyPipe } from '@angular/common';
+import { MatTableModule } from '@angular/material/table';
 
 describe('ProcessedOrdersComponent', () => {
   let component: ProcessedOrdersComponent;
@@ -7,7 +10,13 @@ describe('ProcessedOrdersComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [ProcessedOrdersComponent],
+      imports: [
+        ProcessedOrdersComponent,
+        CommonModule,
+        MatTableModule,
+        CurrencyPipe,
+        MODULES,
+      ],
     }).compileComponents();
 
     fixture = TestBed.createComponent(ProcessedOrdersComponent);
--- libs/frontend/dashboard/src/lib/purchase-orders-breakdown/purchase-orders-breakdown.component.ts
+++ libs/frontend/dashboard/src/lib/purchase-orders-breakdown/purchase-orders-breakdown.component.ts
@@ -17,35 +17,6 @@ import { MatTableModule } from '@angular/material/table';
 import { MatExpansionModule } from '@angular/material/expansion';
 import { MatIconModule } from '@angular/material/icon';
 
-interface Transaction {
-  item: string;
-  cost: number;
-}
-
-interface FoodNode {
-  name: string;
-  children?: FoodNode[];
-}
-
-const TREE_DATA: FoodNode[] = [
-  {
-    name: 'Fruit',
-    children: [{ name: 'Apple' }, { name: 'Banana' }, { name: 'Fruit loops' }],
-  },
-  {
-    name: 'Vegetables',
-    children: [
-      {
-        name: 'Green',
-        children: [{ name: 'Broccoli' }, { name: 'Brussels sprouts' }],
-      },
-      {
-        name: 'Orange',
-        children: [{ name: 'Pumpkins' }, { name: 'Carrots' }],
-      },
-    ],
-  },
-];
 
 @Component({
   selector: 'moofy-purchase-orders-breakdown',
@@ -66,26 +37,12 @@ const TREE_DATA: FoodNode[] = [
 export class PurchaseOrdersBreakdownComponent {
   readonly panelOpenState = signal(false);
   displayedColumns: string[] = ['item', 'cost'];
-  transactions: Transaction[] = [
-    { item: 'Beach ball', cost: 4 },
-    { item: 'Towel', cost: 5 },
-    { item: 'Frisbee', cost: 2 },
-    { item: 'Sunscreen', cost: 4 },
-    { item: 'Cooler', cost: 25 },
-    { item: 'Swim suit', cost: 15 },
-  ];
+
 
   private _bottomSheetRef =
     inject<MatBottomSheetRef<PurchaseOrdersBreakdownComponent>>(
       MatBottomSheetRef
     );
-
-  dataSource = TREE_DATA;
-
-  childrenAccessor = (node: FoodNode) => node.children ?? [];
-
-  hasChild = (_: number, node: FoodNode) =>
-    !!node.children && node.children.length > 0;
 
   constructor(
     @Inject(MAT_BOTTOM_SHEET_DATA) public data: { routePurchaseOrders: any }
