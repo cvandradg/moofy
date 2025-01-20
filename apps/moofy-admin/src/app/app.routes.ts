336e47e57be9337e11b5695d11f9494cffe44ff6
claudio <54946311+cvandradg@users.noreply.github.com>
2025-01-20 10:34:52 -0600

    new project

--- apps/moofy-admin/src/app/app.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Route } from '@angular/router';
-
-export const appRoutes: Route[] = [
-  {
-    path: '',
-    loadChildren: () =>
-      import('@moofy-admin/dashboard').then((m) => m.dashboardRoutes),
-  },
-];
--- libs/frontend/shared/README.md
+++
@@ -1,7 +0,0 @@
-# frontend-moofy-admin-shared
-
-This library was generated with [Nx](https://nx.dev).
-
-## Running unit tests
-
-Run `nx test frontend-moofy-admin-shared` to execute the unit tests.
