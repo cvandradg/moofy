2867c329c21af5321b9764e34773dbfc5ff35e9f
claudio <54946311+cvandradg@users.noreply.github.com>
2025-06-23 01:42:56 -0600

    feat: attempt to fix routes in ssr app

--- apps/moofy-admin/src/app/app.routes.ts
+++ apps/moofy-admin/src/app/app.routes.ts
@@ -3,10 +3,6 @@ import { Route } from '@angular/router';
 export const appRoutes: Route[] = [
   { path: '', pathMatch: 'full', redirectTo: 'subir-ordenes' },
   {
-    path: 'purchase-orders',
-    loadChildren: () => import('@moofy-admin/purchase-orders').then((m) => m.purchaseOrdersRoutes),
-  },
-  {
     path: '',
     loadChildren: () => import('@moofy-admin/dashboard').then((m) => m.dashboardRoutes),
   },
